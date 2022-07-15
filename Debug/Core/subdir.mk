e3e0e1a7817a18f7672ec696202d28dcf22b7b16
DRC-ThanhHai <80090701+DRC-ThanhHai@users.noreply.github.com>
2022-07-15 18:49:12 +0700

    Add files via upload

---
+++ Debug/Core/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Button_Interface_DoAnVXL.c 
+
+OBJS += \
+./Core/Button_Interface_DoAnVXL.o 
+
+C_DEPS += \
+./Core/Button_Interface_DoAnVXL.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Button_Interface_DoAnVXL.o: ../Core/Button_Interface_DoAnVXL.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DSTM32F103xB -DDEBUG -c -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Core/Button_Interface_DoAnVXL.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
